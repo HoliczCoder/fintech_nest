151040315cd82a9b40112c4637062922bc999bde
Minh <minhtranconglis@gmail.com>
2023-04-24 22:42:30 +0700

    update

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
+	constructor(private prismaService: PrismaService) {}
 
-  findAll() {
-    return `This action returns all product`;
-  }
+	create(createProductDto: CreateProductDto) {
+		return 'This action adds a new product';
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+	findAll() {
+		return `This action returns all product`;
+	}
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} product`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+	update(id: number, updateProductDto: UpdateProductDto) {
+		return `This action updates a #${id} product`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} product`;
+	}
 }
--- src/user/customer.service.ts
+++ src/user/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Customer, Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class CustomerService {
@@ -11,5 +12,19 @@ export class CustomerService {
 			where: customerWhereUniqueInput
 		});
 		return result;
+	}
+
+	async createCustomer(email: string, full_name: string, password: string) {
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password, salt);
+		return await this.prisma.customer.create({
+			data: {
+				full_name: full_name,
+				email: email,
+				password: hash,
+				group_id: 1,
+				status: 1
+			}
+		});
 	}
 }
